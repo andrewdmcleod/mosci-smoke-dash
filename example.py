979b6e93fece8cb86365acfe9be3bf3d34151cf8
Rhys Powell <rhys.powell@gmail.com>
2019-03-29 22:00:40 +0000

    updated

--- example.py
+++ example.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ugf uyfyitdy fougiuf iytfciygvc iygcycyi'
 # app.debug = env['app.debug']
 
-data = {"140": {"arch": "amd64", "type": "openstack-telemetry", "ubuntu release": "xenial", "openstack release": "queens", "tempest result": "setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n"}, "139": {"arch": "amd64", "type": "openstack-base", "ubuntu release": "xenial", "openstack release": "ocata", "tempest result": "setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n"}}
+data = {'140': {'arch': 'amd64', 'type': 'openstack-telemetry', 'ubuntu release': 'xenial', 'openstack release': 'queens', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '139': {'arch': 'amd64', 'type': 'openstack-base', 'ubuntu release': 'xenial', 'openstack release': 'ocata', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}}
 
 
 @app.route('/')
 def index():
     # for x in data:
-    
-    return render_template('layout.html', data=data.json())
+    for key, value in data.items():
+        print(key, value)
+    return render_template('layout.html', data=data)
 
 
 
