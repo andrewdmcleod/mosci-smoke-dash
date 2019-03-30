7566ebb89a379b58d2e82c7cc72f563e02e4d22d
Andrew McLeod <andrew.mcleod@canonical.com>
2019-03-30 18:12:30 +0100

    add row colours

--- example.py
+++ example.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ugf uyfyitdy fougiuf iytfciygvc iygcycyi'
 # app.debug = env['app.debug']
 
-DATA = {'140': {'arch': 'amd64', 'type': 'openstack-telemetry', 'ubuntu release': 'xenial', 'openstack release': 'queens', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '139': {'arch': 'amd64', 'type': 'openstack-base', 'ubuntu release': 'xenial', 'openstack release': 'ocata', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '138': {'arch': 'ARMv5', 'type': 'openstack-base', 'ubuntu release': 'xenial', 'openstack release': 'ocata', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '137': {'arch': 'ARMv7', 'type': 'openstack-telemetry', 'ubuntu release': 'xenial', 'openstack release': 'queens', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}}
+# DATA = {'140': {'arch': 'amd64', 'type': 'openstack-telemetry', 'ubuntu release': 'xenial', 'openstack release': 'queens', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '139': {'arch': 'amd64', 'type': 'openstack-base', 'ubuntu release': 'xenial', 'openstack release': 'ocata', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '138': {'arch': 'ARMv5', 'type': 'openstack-base', 'ubuntu release': 'xenial', 'openstack release': 'ocata', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\nsetUpClass (tempest.api.object_storage.test_account_services.AccountTest)\nsetUpClass (tempest.api.object_storage.test_container_services.ContainerTest)\nsetUpClass (tempest.api.object_storage.test_account_quotas.AccountQuotasTest)\nsetUpClass (tempest.api.object_storage.test_container_quotas.ContainerQuotasTest)\nsetUpClass (tempest.api.object_storage.test_object_services.ObjectTest)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}, '137': {'arch': 'ARMv7', 'type': 'openstack-telemetry', 'ubuntu release': 'xenial', 'openstack release': 'queens', 'tempest result': 'setUpClass (tempest.api.compute.servers.test_create_server.ServersTestBootFromVolume)\ntempest.api.volume.test_volumes_get.VolumesGetTest.test_volume_create_get_update_delete_from_image[id-54a01030-c7fc-447c-86ee-c1182beae638,image,smoke]\n'}}
 
+with open('results.json') as json_file:
+    DATA = json.load(json_file)
 
 @app.route('/')
 def index():
@@ -22,16 +24,16 @@ def arch():
     # default dict from collections would work here to make it all dynamic
     global DATA
     amd64 = []
-    ARMv5 = []
-    ARMv7 = []
+    arm64 = []
+    ppc64el = []
     for key, value in DATA.items():
         if value['arch'] == 'amd64':
             amd64.append(value)
-        elif value['arch'] == 'ARMv5':
-            ARMv5.append(value)
-        elif value['arch'] == 'ARMv7':
-            ARMv7.append(value)
-    data = [amd64, ARMv5, ARMv7]
+        elif value['arch'] == 'arm64':
+            arm64.append(value)
+        elif value['arch'] == 'ppc64el':
+            ppc64el.append(value)
+    data = [amd64, arm64, ppc64el]
     print(data)
     return render_template('arch.html', data=data)
 
